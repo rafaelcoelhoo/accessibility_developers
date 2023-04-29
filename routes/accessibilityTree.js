8063f048ed6da6bda25d58a29366a976a758e1a7
rafael.coelho <rafaelcoelho7@hotmail.com>
2023-04-29 20:29:37 +0100

    Add two accessibility tree versions

--- routes/accessibilityTree.js
+++ routes/accessibilityTree.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 /* GET Accessibility Tree page. */
 router.get('/good', function (req, res) {
-  res.render('accessibilityTree/good/accessibilityTree');
+  res.render('accessibilityTree/good/accessibilityTree', {title: "Árvore acessibilidade"});
+});
+
+router.get('/bad', function (req, res) {
+  res.render('accessibilityTree/bad/accessibilityTree', {title: "Árvore acessibilidade"});
 });
 
 export {router as accessibilityTreeRouter};
